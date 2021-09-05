d17d17eb44689ba85f1e8ca0aa68c93b532d7e3b
Maleke Thomas <malekelthomas97@gmail.com>
2021-09-05 18:23:46 -0400

    log connection, env config

--- pkg/store/postgres/postresgorm.go
+++ pkg/store/postgres/postresgorm.go
@@ -1,6 +1,9 @@
 package postgres
 
 import (
+	"log"
+
+	"github.com/malekelthomas/ShelterCM-API/pkg/config"
 	"github.com/malekelthomas/ShelterCM-API/pkg/store"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,7 +14,8 @@ var dbConn store.GormDBConn
 var gdb *gorm.DB
 
 func init() {
-	dsn := ""
+	dsn := config.Config.GetDSN()
+	log.Printf("Opening db connection: %s\n", dsn)
 	gormDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
