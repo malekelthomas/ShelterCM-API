80ad67ac46812defeb17cbcd2d1f19eee5c97ae5
leke <malekelthomas97@gmail.com>
2021-09-06 01:26:10 -0400

    upload signature/qr to storage bucket

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/malekelthomas/ShelterCM-API
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go v1.40.37
 	github.com/go-chi/chi/v5 v5.0.4
-	github.com/sethvargo/go-envconfig v0.3.5 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/sethvargo/go-envconfig v0.3.5
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.14
 )
--- pkg/store/postgres/signature.go
+++ pkg/store/postgres/signature.go
@@ -1,9 +1,16 @@
 package postgres
 
 import (
+	"bytes"
+	"fmt"
 	"sync"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/malekelthomas/ShelterCM-API/pkg/config"
 	"github.com/malekelthomas/ShelterCM-API/pkg/signature"
+	"github.com/skip2/go-qrcode"
 	"gorm.io/gorm"
 )
 
@@ -14,7 +21,9 @@ type SignatureStore struct {
 func NewSignatureStore() *SignatureStore {
 	var o sync.Once
 	o.Do(func() {
-		gdb.AutoMigrate(&signature.Signature{})
+		if err := gdb.AutoMigrate(&signature.Signature{}); err != nil {
+			panic(err)
+		}
 	})
 	return &SignatureStore{
 		db: gdb,
@@ -22,6 +31,25 @@ func NewSignatureStore() *SignatureStore {
 }
 
 func (ss *SignatureStore) Create(signature *signature.Signature) (*signature.Signature, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	uploader := s3manager.NewUploader(sess)
+	png, err := qrcode.Encode(signature.UserID.String(), qrcode.Highest, 256)
+	if err != nil {
+		return nil, err
+	}
+	r := bytes.NewReader(png)
+	s3res, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: &config.Config.AWS.SignatureBucket,
+		Key:    aws.String(fmt.Sprintf("signatures/%s-%v.png", signature.UserID.String(), signature.SignatureType)),
+		Body:   r,
+	})
+	if err != nil {
+		return nil, err
+	}
+	signature.Source = s3res.Location
+
 	result := ss.db.Create(&signature)
 	if result.Error != nil {
 		return nil, result.Error
